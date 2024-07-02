d4ed3b8af3cba4ba524b74240ff4d9c321eb1d6d
otanikok <kavigon114@gmail.com>
2024-07-02 17:15:56 +0900

    記事の削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,11 +9,41 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
     
     public function show(Post $post)
     {
-        dd($post);
+        return view('posts.show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,14 +10,29 @@
    </head>
    <body class="antialiased">
        <h1>Blog Name</h1>
+       <a href="/posts/cretate">create</a>
        <div class='posts'>
            @foreach($posts as $post)
                 <div class='post'>
                     <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
                     <p class='body'>{{ $post->body }}</p>
+                    <from action="/posts/{{ $post->id }}" id="from_{{ $post->id }}"method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                   </from>
                 </div>
            @endforeach
        </div>
        <div class='paginate'>{{ $posts->links()}}</div>
+       <script>
+           function deletePost(id) {
+               'use strict'
+               
+               if(confirm('削除すると復元できません。\本当に削除しますか？')) {
+                   document.getElementById(`form_${id}`).submit();
+               }
+           }
+       </script>
    </body>
 </html>
